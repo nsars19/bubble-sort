b856ea454f49295a0b9e88aa961d81791daf0b6f
Nick <nsardonini@gmail.com>
2020-03-26 19:17:41 -0400

    add #bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,4 +13,31 @@ def bubble_sort arr
   puts "#{arr}"
 end
 
-puts bubble_sort(array_generator(10))
+#puts bubble_sort(array_generator(10))
+
+def bubble_sort_by arr
+ 
+  (arr.length - 1).times do
+    arr.each_with_index do |e, i|
+
+      next if e == arr.last
+      if yield(arr[i], arr[i + 1]) == 1
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      end
+     
+      i += 1
+    end
+  end
+  puts "#{arr}"
+end
+
+test_array = [6, 19, 75, 4, 16, 29, 93, 5, 10, 13, 68]
+test_two = [3, 2, 1]
+
+bubble_sort_by(test_two) do |left, right|
+  left <=> right
+end
+bubble_sort_by(test_array) do |left, right|
+  left <=> right
+end
+
