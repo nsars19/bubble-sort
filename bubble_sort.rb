c2e219986f5c3529160f59355c9cb5dfd96712f9
Nick <nsardonini@gmail.com>
2020-03-26 18:16:34 -0400

    add #bubble_sort method

---
+++ bubble_sort.rb
@@ -0,0 +1,15 @@
+require '~/ruby_learnins/Ruby_Exercises/stock_picker.rb'
+# using #array_generator method to provide random arrays for testing
+
+def bubble_sort arr
+
+  (arr.length - 1).times do
+    arr.each_with_index do |e, i|
+      next if e == arr.last
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if e > arr[i + 1]
+      puts "#{arr}"
+    end
+  end
+end
+bubble_sort (array_generator(5).shuffle)
+#puts bubble_sort(array_generator(10).shuffle)
